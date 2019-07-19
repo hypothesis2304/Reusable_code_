e71865d6e9195422599a0cc6a92aaca2cfa99a03
Bhadrinath Nagabandi (Student) <bnagaban@asu.edu>
2019-07-18 23:52:44 -0700

    changed resnet, added base_network

--- network.py
+++ network.py
@@ -8,18 +8,6 @@ import imgaug.augmenters as iaa
 import math
 import pdb
 
-def init_weights(m):
-    classname = m.__class__.__name__
-    if classname.find('Conv2d') != -1 or classname.find('ConvTranspose2d') != -1:
-        nn.init.kaiming_uniform_(m.weight)
-        nn.init.zeros_(m.bias)
-    elif classname.find('BatchNorm') != -1:
-        nn.init.normal_(m.weight, 1.0, 0.02)
-        nn.init.zeros_(m.bias)
-    elif classname.find('Linear') != -1:
-        nn.init.xavier_normal_(m.weight)
-        nn.init.zeros_(m.bias)
-
 def aToBSheduler(step, A, B, gamma=10, max_iter=10000):
     ans = A + (2.0 / (1 + np.exp(- gamma * step * 1.0 / max_iter)) - 1.0) * (B - A)
     return float(ans)
@@ -49,62 +37,76 @@ class GradientReverseModule(nn.Module):
             self.global_step += 1.0
         return self.grl(self.coeff, x)
 
-class ResNetFc(nn.Module):
-  def __init__(self, resnet_name, use_bottleneck=True, bottleneck_dim=256, new_cls=False, class_num=1000):
-    super(ResNetFc, self).__init__()
-    model_resnet = resnet_dict[resnet_name](pretrained=True)
-    self.conv1 = model_resnet.conv1
-    self.bn1 = model_resnet.bn1
-    self.relu = model_resnet.relu
-    self.maxpool = model_resnet.maxpool
-    self.layer1 = model_resnet.layer1
-    self.layer2 = model_resnet.layer2
-    self.layer3 = model_resnet.layer3
-    self.layer4 = model_resnet.layer4
-    self.avgpool = model_resnet.avgpool
-    self.feature_layers = nn.Sequential(self.conv1, self.bn1, self.relu, self.maxpool, \
-                         self.layer1, self.layer2, self.layer3, self.layer4, self.avgpool)
+from torchvision import models
 
-    self.use_bottleneck = use_bottleneck
-    self.new_cls = new_cls
-    if new_cls:
-        if self.use_bottleneck:
-            self.bottleneck = nn.Linear(model_resnet.fc.in_features, bottleneck_dim)
-            self.fc = nn.Linear(bottleneck_dim, class_num)
-            self.bottleneck.apply(init_weights)
-            self.fc.apply(init_weights)
-            self.__in_features = bottleneck_dim
+
+class BaseFeatureExtractor(nn.Module):
+    def forward(self, *input):
+        pass
+
+    def __init__(self):
+        super(BaseFeatureExtractor, self).__init__()
+
+    def output_num(self):
+        pass
+
+    def train(self, mode=True):
+        for module in self.children():
+            if isinstance(module, nn.BatchNorm2d):
+                module.train(False)
+            else:
+                module.train(mode)
+
+
+class ResNet50Fc(BaseFeatureExtractor):
+    def __init__(self,model_path=None, normalize=True):
+        super(ResNet50Fc, self).__init__()
+        if model_path:
+            if os.path.exists(model_path):
+                self.model_resnet = models.resnet50(pretrained=False)
+                self.model_resnet.load_state_dict(torch.load(model_path))
+            else:
+                raise Exception('invalid model path!')
         else:
-            self.fc = nn.Linear(model_resnet.fc.in_features, class_num)
-            self.fc.apply(init_weights)
-            self.__in_features = model_resnet.fc.in_features
-    else:
-        self.fc = model_resnet.fc
+            self.model_resnet = models.resnet50(pretrained=True)
+
+        if model_path or normalize:
+            # pretrain model is used, use ImageNet normalization
+            self.normalize = True
+            self.register_buffer('mean', torch.tensor([0.485, 0.456, 0.406]).view(1, 3, 1, 1))
+            self.register_buffer('std', torch.tensor([0.229, 0.224, 0.225]).view(1, 3, 1, 1))
+        else:
+            self.normalize = False
+
+        model_resnet = self.model_resnet
+        self.conv1 = model_resnet.conv1
+        self.bn1 = model_resnet.bn1
+        self.relu = model_resnet.relu
+        self.maxpool = model_resnet.maxpool
+        self.layer1 = model_resnet.layer1
+        self.layer2 = model_resnet.layer2
+        self.layer3 = model_resnet.layer3
+        self.layer4 = model_resnet.layer4
+        self.avgpool = model_resnet.avgpool
         self.__in_features = model_resnet.fc.in_features
 
-  def forward(self, x):
-    x = self.feature_layers(x)
-    x = x.view(x.size(0), -1)
-    if self.use_bottleneck and self.new_cls:
-        x = self.bottleneck(x)
-    y = self.fc(x)
-    return x, y
+    def forward(self, x):
+        if self.normalize:
+            x = (x - self.mean) / self.std
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        return x
 
-  def output_num(self):
-    return self.__in_features
-
-  def get_parameters(self):
-    if self.new_cls:
-        if self.use_bottleneck:
-            parameter_list = [{"params":self.feature_layers.parameters(), "lr_mult":1, 'decay_mult':2}, \
-                            {"params":self.bottleneck.parameters(), "lr_mult":10, 'decay_mult':2}, \
-                            {"params":self.fc.parameters(), "lr_mult":10, 'decay_mult':2}]
-        else:
-            parameter_list = [{"params":self.feature_layers.parameters(), "lr_mult":1, 'decay_mult':2}, \
-                            {"params":self.fc.parameters(), "lr_mult":10, 'decay_mult':2}]
-    else:
-        parameter_list = [{"params":self.parameters(), "lr_mult":1, 'decay_mult':2}]
-    return parameter_list
+    def output_num(self):
+        return self.__in_features
 
 
 class AdversarialNetwork(nn.Module):
